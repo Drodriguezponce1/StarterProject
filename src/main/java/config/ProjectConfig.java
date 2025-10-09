9b1d90310d77968bbc49e5583319a425cd8e11b0
Drodriguezponce1 <Danieljrod98@yahoo.com>
2025-10-09 13:32:22 -0700

    Learned how to handle different Beans depending on the method name for the context

--- src/main/java/config/ProjectConfig.java
+++ src/main/java/config/ProjectConfig.java
@@ -7,11 +7,38 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ProjectConfig {
 
+    /* I have learned that this can be automated later once we get to the Spring sections.
+     *  Spring now has a @SpringApplication that handles most of this, but this is really cool
+     *
+     *
+     *  A Bean is essentially a class or object defined to be under the 'context' of Spring
+     * */
+
     @Bean
     Person person(){
         Person p = new Person();
         p.setName("Danny");
         p.setAge(99);
         return p;
+    }
+
+    @Bean
+    Person danny() {
+        return new Person("Danny", 77);
+    }
+
+    @Bean
+    String randomWord() {
+        return "Lalalalala";
+    }
+
+    @Bean
+    String hello() {
+        return "hello";
+    }
+
+    @Bean
+    Integer ten() {
+        return 10;
     }
 }
--- src/main/java/main/Driver.java
+++ src/main/java/main/Driver.java
@@ -8,8 +8,12 @@ public class Driver {
 
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
-        Person p = context.getBean(Person.class);
+        Person p = context.getBean("person", Person.class);
+        Person d = context.getBean("danny", Person.class);
+        String random = context.getBean("hello", String.class);
 
+        System.out.println(random);
         System.out.println(p);
+        System.out.println(d);
     }
 }
