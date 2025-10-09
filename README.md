521c2ee8e25af189816d50a7c29d8c197b36b9f6
Drodriguezponce1 <Danieljrod98@yahoo.com>
2025-10-09 11:40:34 -0700

    Started this project learning Spring. I am reading the Spring Start Here Book

---
+++ README.md
@@ -0,0 +1,3 @@
+Started this project while reading "Spring Starts Here".
+
+- I will try and make my own touch when it comes to some of the projects
---
+++ src/main/java/main/Driver.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Driver {
+    public static void main(String[] args) {
+        System.out.println("Just wanted to say hello");
+
+        for(int i  = Integer.parseInt(args[0]); i <= Integer.parseInt(args[1]); i++){
+            System.out.println(i);
+        }
+    }
+}
