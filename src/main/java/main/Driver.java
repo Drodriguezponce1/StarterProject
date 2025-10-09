3e42da4f0333eb4eebc597d779496a1628a95d85
Drodriguezponce1 <Danieljrod98@yahoo.com>
2025-10-09 11:45:31 -0700

    Just github test

--- src/main/java/main/Driver.java
+++ src/main/java/main/Driver.java
@@ -4,7 +4,7 @@ public class Driver {
     public static void main(String[] args) {
         System.out.println("Just wanted to say hello");
 
-        for(int i  = Integer.parseInt(args[0]); i <= Integer.parseInt(args[1]); i++){
+        for(int i  = Integer.parseInt(args[0]); i <= Integer.parseInt(args[1]) + 1; i++){
             System.out.println(i);
         }
     }
